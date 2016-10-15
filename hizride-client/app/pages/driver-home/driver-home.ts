57cc0dd572e455e87bf6ad94c86bbbb8394d1cd0
Sasu Mäkinen <sasumaki@dhcp-asennus-kumpula-28.helsinki.fi>
2016-10-15 20:20:05 +0300

    palauttaa reitin drivernäkymässä

--- hizride-client/app/pages/driver-home/driver-home.ts
+++ hizride-client/app/pages/driver-home/driver-home.ts
@@ -16,6 +16,7 @@ export class DriverHomePage implements OnInit{
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   toValue:string;
+
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
  	this.toValue = "";
   }
@@ -25,6 +26,8 @@ export class DriverHomePage implements OnInit{
   }
 
   loadMap(){
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
 
     Geolocation.getCurrentPosition().then((position) => {
 
@@ -38,8 +41,11 @@ export class DriverHomePage implements OnInit{
 
 	  // create the map itself
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-	  
-	  // get the two fields
+
+
+      directionsDisplay.setMap(this.map);
+
+      // get the two fields
 	  let input_to = (<HTMLInputElement>document.getElementById("journey_to"));
 
 	  // set the autocomplete options
@@ -62,7 +68,7 @@ export class DriverHomePage implements OnInit{
 		  let place = autocomplete.getPlace();
 		  let geometry = place.geometry;
 		  self.map.setCenter({ lat: -33.8688, lng: 151.2195 })
-		  
+
 		var bounds = new google.maps.LatLngBounds();
 
 		if ((geometry) !== undefined) {
@@ -70,7 +76,7 @@ export class DriverHomePage implements OnInit{
 		  console.log(place.name);
 		  console.log(geometry.location.lng());
 		  console.log(geometry.location.lat());
-		
+
 		  var icon = {
 			  url: place.icon,
 			  size: new google.maps.Size(71, 71),
@@ -87,15 +93,28 @@ export class DriverHomePage implements OnInit{
 			  animation: google.maps.Animation.DROP,
 			  position: place.geometry.location,
 		  });
-		
-		  if (place.geometry.viewport) {
+
+		  /*if (place.geometry.viewport) {
 			  bounds.union(place.geometry.viewport);
 			  } else {
 			  bounds.extend(place.geometry.location);
 			  }
-			  self.map.fitBounds(bounds);
+			  */
+      var request = {
+        origin: latLng,
+        destination: place.geometry.location,
+        travelMode: 'DRIVING'
+      };
+      directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(result);
+        }
+      });
+
+     // self.map.fitBounds(bounds);
+
 		   }
-		   
+
 		});
 
 	}, (err) => {
