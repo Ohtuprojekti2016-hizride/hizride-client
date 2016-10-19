46b28bc340c6408667852d6582e37d69669af0fc
Smobey <smobey@gmail.com>
2016-10-19 16:19:46 +0300

    Reitti fiksattu

--- hizride-client/app/pages/driver-home/driver-home.ts
+++ hizride-client/app/pages/driver-home/driver-home.ts
@@ -17,9 +17,9 @@ export class DriverHomePage implements OnInit{
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   toValue:string;
+
   constructor(public navCtrl: NavController, private platform: Platform, public alertCtrl: AlertController, public user: User) {
  	this.toValue = "";
- 	this.platform = platform;
   }
 
   ionViewLoaded(){
@@ -27,11 +27,11 @@ export class DriverHomePage implements OnInit{
   }
 
   loadMap(){
-  console.log("1");
-  	this.platform.ready().then(() => {
-  	console.log("1,5" + this.platform);
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+
     Geolocation.getCurrentPosition().then((position) => {
-    console.log("2");
+
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
       let mapOptions = {
@@ -42,8 +42,11 @@ export class DriverHomePage implements OnInit{
 
 	  // create the map itself
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-	  
-	  // get the two fields
+
+
+      directionsDisplay.setMap(this.map);
+
+      // get the two fields
 	  let input_to = (<HTMLInputElement>document.getElementById("journey_to"));
 
 	  // set the autocomplete options
@@ -66,7 +69,7 @@ export class DriverHomePage implements OnInit{
 		  let place = autocomplete.getPlace();
 		  let geometry = place.geometry;
 		  self.map.setCenter({ lat: -33.8688, lng: 151.2195 })
-		  
+
 		var bounds = new google.maps.LatLngBounds();
 
 		if ((geometry) !== undefined) {
@@ -74,7 +77,7 @@ export class DriverHomePage implements OnInit{
 		  console.log(place.name);
 		  console.log(geometry.location.lng());
 		  console.log(geometry.location.lat());
-		
+
 		  var icon = {
 			  url: place.icon,
 			  size: new google.maps.Size(71, 71),
@@ -91,21 +94,34 @@ export class DriverHomePage implements OnInit{
 			  animation: google.maps.Animation.DROP,
 			  position: place.geometry.location,
 		  });
-		
-		  if (place.geometry.viewport) {
+
+		  /*if (place.geometry.viewport) {
 			  bounds.union(place.geometry.viewport);
 			  } else {
 			  bounds.extend(place.geometry.location);
 			  }
-			  self.map.fitBounds(bounds);
+			  */
+      var request = {
+        origin: latLng,
+        destination: place.geometry.location,
+        travelMode: 'DRIVING'
+      };
+      directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(result);
+        }
+      });
+
+     // self.map.fitBounds(bounds);
+
 		   }
-		   
+
 		});
 
 	}, (err) => {
 	  console.log(err);
 	});
-	});
+
 	  }
 
 addMarker(){
@@ -137,6 +153,8 @@ addInfoWindow(marker, content){
 ngOnInit(){
 
 }
+
+
 
 showConfirm() {
 	var name = this.user.social.facebook.data.full_name;
