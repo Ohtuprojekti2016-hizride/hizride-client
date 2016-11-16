b9b75e9160fa0a14dc17f3c76a14c541ca7f2b70
Smobey <smobey@gmail.com>
2016-11-16 14:23:07 +0200

    polyline

--- hizride-client/src/pages/driver-home/driver-home.ts
+++ hizride-client/src/pages/driver-home/driver-home.ts
@@ -12,124 +12,138 @@ declare var google;
   templateUrl: 'driver-home.html'
 })
 
-export class DriverHomePage implements OnInit{
+export class DriverHomePage implements OnInit {
 
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
-  toValue:string;
+	@ViewChild('map') mapElement: ElementRef;
+	map: any;
+	toValue:string;
 
-  constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController, public user: User) {
- 	this.toValue = "";
-  this.platform = platform;
-  }
+	constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController, public user: User) {
+		this.toValue = "";
+		this.platform = platform;
+	}
 
-  ionViewDidLoad(){
-    this.loadMap();
-  }
+	ionViewDidLoad() {
+		this.loadMap();
+	}
 
-  loadMap(){
-  console.log("0");
-    this.platform.ready().then(() => {
-    console.log("1" + this.platform);
-    var directionsService = new google.maps.DirectionsService();
-    console.log("2");
+	loadMap() {
+		console.log("0");
+		
+		this.platform.ready().then(() => {
+			var directionsService = new google.maps.DirectionsService();
+			var directionsDisplay = new google.maps.DirectionsRenderer();
 
-    var directionsDisplay = new google.maps.DirectionsRenderer();
+			Geolocation.getCurrentPosition({timeout: 30000, enableHighAccuracy: false}).then((position) => {
 
-    Geolocation.getCurrentPosition({timeout: 30000, enableHighAccuracy: false}).then((position) => {
+				let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-console.log("3");
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+				let mapOptions = {
+					center: latLng,
+					zoom: 15,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				}
 
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
+				// create the map itself
+				this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+				directionsDisplay.setMap(this.map);
 
-	  // create the map itself
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+				// get he two fields
+				let input_to = (<HTMLInputElement>document.getElementById("journey_to"));
 
+				// set the autocomplete options
+				let autocompleteOptions = {
+					types: [],
+					componentRestrictions: {country: "fi"}
+				};
 
-      directionsDisplay.setMap(this.map);
+				// create the two autocompletes on the from and to fields
+				let autocomplete = new google.maps.places.Autocomplete(input_to, autocompleteOptions);
 
-      // get the two fields
-	  let input_to = (<HTMLInputElement>document.getElementById("journey_to"));
+				// we need to save a reference to this as we lose it in the callbacks
+				let self = this;
 
-	  // set the autocomplete options
-	  let autocompleteOptions = {
-		types: [],
-		componentRestrictions: {country: "fi"}
-	  };
+				// add the first listener
+				// TODO
 
-	  // create the two autocompletes on the from and to fields
-	  let autocomplete = new google.maps.places.Autocomplete(input_to, autocompleteOptions);
+				// add the second listener
+				google.maps.event.addListener(autocomplete, 'place_changed', function() {
+					let place = autocomplete.getPlace();
+					let geometry = place.geometry;
+					self.map.setCenter({ lat: -33.8688, lng: 151.2195 })
 
-	  // we need to save a reference to this as we lose it in the callbacks
-	  let self = this;
+					var bounds = new google.maps.LatLngBounds();
 
-	  // add the first listener
-	  // TODO
+					if ((geometry) !== undefined) {
 
-	  // add the second listener
-	  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-		  let place = autocomplete.getPlace();
-		  let geometry = place.geometry;
-		  self.map.setCenter({ lat: -33.8688, lng: 151.2195 })
+						console.log(place.name);
+						console.log(geometry.location.lng());
+						console.log(geometry.location.lat());
 
-		var bounds = new google.maps.LatLngBounds();
-
-		if ((geometry) !== undefined) {
-
-		  console.log(place.name);
-		  console.log(geometry.location.lng());
-		  console.log(geometry.location.lat());
-
-		  var icon = {
-			  url: place.icon,
-			  size: new google.maps.Size(71, 71),
-			  origin: new google.maps.Point(0, 0),
-			  anchor: new google.maps.Point(17, 34),
-			  scaledSize: new google.maps.Size(25, 25)
-		  };
+						var icon = {
+							url: place.icon,
+							size: new google.maps.Size(71, 71),
+							origin: new google.maps.Point(0, 0),
+							anchor: new google.maps.Point(17, 34),
+							scaledSize: new google.maps.Size(25, 25)
+						};
 
 
-		  let marker = new google.maps.Marker({
-			  map: self.map,
-			  icon: icon,
-			  title: place.name,
-			  animation: google.maps.Animation.DROP,
-			  position: place.geometry.location,
-		  });
+						let marker = new google.maps.Marker({
+							map: self.map,
+							icon: icon,
+							title: place.name,
+							animation: google.maps.Animation.DROP,
+							position: place.geometry.location,
+						});
 
-		  /*if (place.geometry.viewport) {
-			  bounds.union(place.geometry.viewport);
-			  } else {
-			  bounds.extend(place.geometry.location);
-			  }
-			  */
-      var request = {
-        origin: latLng,
-        destination: place.geometry.location,
-        travelMode: 'DRIVING'
-      };
-      directionsService.route(request, function(result, status) {
-        if (status == 'OK') {
-          directionsDisplay.setDirections(result);
-        }
-      });
+						/*if (place.geometry.viewport) {
+						bounds.union(place.geometry.viewport);
+						} else {
+						bounds.extend(place.geometry.location);
+						}
+						*/
+				  
+						var request = {
+							origin: latLng,
+							destination: place.geometry.location,
+							travelMode: 'DRIVING'
+						};
+						
+						var cascadiaFault = new google.maps.Polyline({
+							path: [
+							  new google.maps.LatLng(49.95, -128.1),
+							  new google.maps.LatLng(46.26, -126.3),
+							  new google.maps.LatLng(40.3, -125.4)
+							]
+						});
+						console.log(cascadiaFault);
+						
+						directionsService.route(request, function(result, status) {
+							if (status == 'OK') {
+								directionsDisplay.setDirections(result);
+								let polyline = result.routes["0"].overview_polyline;
+								let newPolyline = new google.maps.Polyline({
+									path:google.maps.geometry.encoding.decodePath(polyline)
+								});
+								
+								if (google.maps.geometry.poly.isLocationOnEdge(latLng, newPolyline, 10e-1)) {
+									alert("Relocate!");
+								}
+								
+							}
+						});
+					
+						
+						//self.map.fitBounds(bounds);
+					}
+				});
 
-     // self.map.fitBounds(bounds);
-
-		   }
-
+			}, (err) => {
+				console.log(err);
+			});
 		});
-
-	}, (err) => {
-	  console.log(err);
-	});
-  });
-	  }
+	}
 
 addMarker(){
 
@@ -143,7 +157,7 @@ addMarker(){
 
   this.addInfoWindow(marker, content);
 
-}
+}	
 
 addInfoWindow(marker, content){
 
